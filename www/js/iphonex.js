a2e13cb7d632717165381bfacdde616e88f8f6ac
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-04-24 09:49:38 +0900

    [Update] iPhone切り欠き対応

---
+++ www/js/iphonex.js
@@ -0,0 +1,5 @@
+if (ons.platform.isIPhoneX()) { // iPhone X であるか否かを判定
+    // <html> 要素に属性を追加（値として空文字列を設定）
+    document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
+    document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
+  }
