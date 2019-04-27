72344603364fe25e524c1003b59691a1b438f4d8
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-04-27 19:50:55 +0900

    [Fix] list

--- www/js/iphonex.js
+++ www/js/iphonex.js
@@ -2,4 +2,4 @@ if (ons.platform.isIPhoneX()) { // iPhone X であるか否かを判定
     // <html> 要素に属性を追加（値として空文字列を設定）
     document.documentElement.setAttribute('onsflag-iphonex-portrait', ''); // 縦
     document.documentElement.setAttribute('onsflag-iphonex-landscape', ''); // 横
-  }
+}
