23039aefea2385957446cc76e8a9885183c8381e
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-04-29 22:32:46 +0900

    [Add] infinite-list

--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,19 @@
 ons.ready(function() {
     console.log("Onsen UI is ready!");
 
+    var infiniteList = document.getElementById('infinite-list');
 
-  });
+    infiniteList.delegate = {
+      createItemContent: function(i) {
+        return ons.createElement('<ons-list-item>Item ' + i + '</ons-list-item>');
+      },
+      countItems: function() {
+        return 10000;
+      }
+    };
+    infiniteList.refresh();
+});
 
-    document.addEventListener('prechange', function(event) {
-      document.querySelector('ons-toolbar .center')
-        .innerHTML = event.tabItem.getAttribute('label');
-    });
+document.addEventListener('prechange', function(event) {
+  document.querySelector('ons-toolbar .center').innerHTML = event.tabItem.getAttribute('label');
+});
