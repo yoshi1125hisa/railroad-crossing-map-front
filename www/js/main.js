ea5732b12c2933a3a1dd919ebb984f8afe788ce4
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-04-30 09:47:19 +0900

    [Update] SlideMenu

--- www/js/main.js
+++ www/js/main.js
@@ -1,18 +1,20 @@
 ons.ready(function() {
     console.log("Onsen UI is ready!");
-
-    var infiniteList = document.getElementById('infinite-list');
-
-    infiniteList.delegate = {
-      createItemContent: function(i) {
-        return ons.createElement('<ons-list-item>Item ' + i + '</ons-list-item>');
-      },
-      countItems: function() {
-        return 10000;
-      }
-    };
-    infiniteList.refresh();
 });
+
+window.fn = {};
+window.fn.open = function() {
+  console.log("menu is open!");
+  var menu = document.getElementById('menu');
+  menu.open();
+};
+window.fn.load = function(page) {
+  var content = document.getElementById('content');
+  var menu = document.getElementById('menu');
+  content
+    .load(page)
+    .then(menu.close.bind(menu));
+};
 
 document.addEventListener('prechange', function(event) {
   document.querySelector('ons-toolbar .center').innerHTML = event.tabItem.getAttribute('label');
