88a2bcb77f38a5540e0daae6eb7c9593e5c490d0
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-05-05 11:36:46 +0900

    [Fix] ストリートビューを非表示

--- www/js/main.js
+++ www/js/main.js
@@ -2,21 +2,6 @@ ons.ready(function() {
     console.log("Onsen UI is ready!");
 });
 
-window.fn = {};
-window.fn.open = function() {
-  console.log("menu is open!");
-  let menu = document.getElementById('menu');
-  menu.open();
-  console.log("menu is opend!")
-};
-window.fn.load = function(page) {
-  let content = document.getElementById('content');
-  let menu = document.getElementById('menu');
-  content
-    .load(page)
-    .then(menu.close.bind(menu));
-};
-
 document.addEventListener('prechange', function(event) {
   document.querySelector('ons-toolbar .center').innerHTML = event.tabItem.getAttribute('label');
 });
