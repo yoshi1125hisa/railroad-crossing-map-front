416ca7c4b75b75f7f79376fd91b11149310a6cd8
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-05-13 08:37:12 +0900

    [Update] ファイル構造整理

--- www/js/map.js
+++ www/js/map.js
@@ -43,7 +43,7 @@ function initMap() {
       position: location,
       // label: locations[i].name,
       icon: {
-        url: 'img/humikiri.png'
+        url: 'img/icon/humikiri.png'
       }
     });
 
