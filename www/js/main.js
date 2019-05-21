df0991a5d401b55e105c9de9153cd052d1de2b1f
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-05-22 08:42:00 +0900

    [Fix] ESLint

--- www/js/main.js
+++ www/js/main.js
@@ -1,14 +1,12 @@
-// eslint-disable-next-line no-undef
 if (ons.platform.isIPhoneX()) { // iPhone X であるか否かを判定
   // <html> 要素に属性を追加（値として空文字列を設定）
   document.documentElement.setAttribute('onsflag-iphonex-portrait', '') // 縦
   document.documentElement.setAttribute('onsflag-iphonex-landscape', '') // 横
 }
 
- // eslint-disable-next-line no-undef
- ons.ready(function () {
-       console.log('Onsen UI is ready!')
- })
+ons.ready(function () {
+  console.log('Onsen UI is ready!')
+})
 
 document.addEventListener('prechange', function (event) {
   document.querySelector('ons-toolbar .center').innerHTML = event.tabItem.getAttribute('label')
@@ -16,6 +14,6 @@ document.addEventListener('prechange', function (event) {
 
 // 画面リロード
 let refreshView = function () {
-  console.log("Hello world!");
-  location.reload();
+  // trueを引数にすることで、WEBサーバーの生データを取得する。 falseではキャッシュから取得。
+  location.reload(true);
 }
