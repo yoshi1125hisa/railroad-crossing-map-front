d9c090e5962c507b45f3c6facda2839e8acc5891
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-05-23 23:19:57 +0900

    [Fix] JS load

--- www/dist/bundle.js
+++ www/dist/bundle.js
@@ -1,40 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-/*!
-  * domready (c) Dustin Diaz 2014 - License MIT
-  */
-!function (name, definition) {
-
-  if (typeof module != 'undefined') module.exports = definition()
-  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
-  else this[name] = definition()
-
-}('domready', function () {
-
-  var fns = [], listener
-    , doc = document
-    , hack = doc.documentElement.doScroll
-    , domContentLoaded = 'DOMContentLoaded'
-    , loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState)
-
-
-  if (!loaded)
-  doc.addEventListener(domContentLoaded, listener = function () {
-    doc.removeEventListener(domContentLoaded, listener)
-    loaded = 1
-    while (listener = fns.shift()) listener()
-  })
-
-  return function (fn) {
-    loaded ? setTimeout(fn, 0) : fns.push(fn)
-  }
-
-});
-
-},{}],2:[function(require,module,exports){
 'use strict';
-
-const domready = require("domready");
-
 
 if (ons.platform.isIPhoneX()) { // iPhone X であるか否かを判定
   // <html> 要素に属性を追加（値として空文字列を設定）
@@ -50,33 +15,28 @@ document.addEventListener('prechange', function (event) {
   document.querySelector('ons-toolbar .center').innerHTML = event.tabItem.getAttribute('label')
 })
 
-domready(function () {
-  exports.something = /* whatever you want */
-  
-  // 画面リロード
-  refreshView = function () {
+// 画面リロード
+let refreshView = function () {
   // trueを引数にすることで、WEBサーバーの生データを取得する。 falseではキャッシュから取得。
   location.reload(true);
 }
 
 // お問い合わせ画面への遷移 (Cordovaプラグインを使ってアプリ版は別ブラウザで起動できるようにする)
 // https://github.com/apache/cordova-plugin-inappbrowser （未実装）
-  contact = function () {
+let contact = function () {
   window.open("https://www.yoshi1125hisa.com/");
 }
 
-  privacyPolicy = function () {
+let privacyPolicy = function () {
   window.open("https://www.yoshi1125hisa.com/");
 }
 
-  oss = function () {
+let oss = function () {
   window.open("https://www.yoshi1125hisa.com/");
 }
 
-  lisence = function () {
+let lisence = function () {
   window.open("https://www.yoshi1125hisa.com/");
 }
 
-});
-
-},{"domready":1}]},{},[2]);
+},{}]},{},[1]);
