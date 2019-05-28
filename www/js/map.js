8a6f0885a584e9f08753978efea26dfe16085a62
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-05-29 08:02:15 +0900

    [Update] 現在地取得コードを追加

--- www/js/map.js
+++ www/js/map.js
@@ -360,6 +360,45 @@ let locations = [
 
 let mapInfo;
 
+
+// 現在地取得処理
+function getPosition() {
+  // 現在地を取得
+  navigator.geolocation.getCurrentPosition(
+    // 取得成功した場合
+    function (position) {
+      alert("緯度:" + position.coords.latitude + ",経度" + position.coords.longitude);
+    },
+    // 取得失敗した場合
+    function (error) {
+      switch (error.code) {
+        case 1: //PERMISSION_DENIED
+          alert("位置情報の利用が許可されていません");
+          break;
+        case 2: //POSITION_UNAVAILABLE
+          alert("現在位置が取得できませんでした");
+          break;
+        case 3: //TIMEOUT
+          alert("タイムアウトになりました");
+          break;
+        default:
+          alert("その他のエラー(エラーコード:" + error.code + ")");
+          break;
+      }
+    }
+  );
+}
+
+    // Geolocation APIに対応している
+    if (navigator.geolocation) {
+      getPosition();
+      // alert("この端末では位置情報が取得できます");
+      // Geolocation APIに対応していない
+    } else {
+      // alert("この端末では位置情報が取得できません");
+    }
+
+
 function initMap() {
   // 地図の作成
   mapInfo = new google.maps.LatLng({
