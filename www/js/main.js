bdd3e15e07a892c57935d800e8f16fb3e3ff8ad5
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-06-04 08:29:39 +0900

    [Update] Platform / Material固定

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Android UI (Material)に固定
+ons.forcePlatformStyling('android');
+
 if (ons.platform.isIPhoneX()) { // iPhone X であるか否かを判定
   // <html> 要素に属性を追加（値として空文字列を設定）
   document.documentElement.setAttribute('onsflag-iphonex-portrait', '') // 縦
