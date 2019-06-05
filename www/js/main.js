6b3a004c1731163f3f4badb59c3ac6a99a434ff1
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-06-06 08:19:41 +0900

    Update Infinity- list

--- www/js/main.js
+++ www/js/main.js
@@ -12,6 +12,18 @@ if (ons.platform.isIPhoneX()) { // iPhone X であるか否かを判定
 ons.ready(function () {
   console.log('Onsen UI is ready!')
   // showTemplateDialog()
+   var infiniteList = document.getElementById('infinite-list');
+
+  infiniteList.delegate = {
+    createItemContent: function(i) {
+      return ons.createElement('<ons-list-item tappable>Item ' + i + '</ons-list-item>');
+    },
+    countItems: function() {
+      return 10000;
+    }
+  };
+
+  infiniteList.refresh();
 })
 
 document.addEventListener('prechange', function (event) {
--- www/js/map.js
+++ www/js/map.js
@@ -370,22 +370,22 @@ function getPosition() {
   navigator.geolocation.getCurrentPosition(
     // 取得成功した場合
     function (position) {
-      alert("緯度:" + position.coords.latitude + ",経度" + position.coords.longitude);
+      // alert("緯度:" + position.coords.latitude + ",経度" + position.coords.longitude);
     },
     // 取得失敗した場合
     function (error) {
       switch (error.code) {
         case 1: //PERMISSION_DENIED
-          alert("位置情報の利用が許可されていません");
+          // alert("位置情報の利用が許可されていません");
           break;
         case 2: //POSITION_UNAVAILABLE
-          alert("現在位置が取得できませんでした");
+          // alert("現在位置が取得できませんでした");
           break;
         case 3: //TIMEOUT
-          alert("タイムアウトになりました");
+          // alert("タイムアウトになりました");
           break;
         default:
-          alert("その他のエラー(エラーコード:" + error.code + ")");
+          // alert("その他のエラー(エラーコード:" + error.code + ")");
           break;
       }
     }
