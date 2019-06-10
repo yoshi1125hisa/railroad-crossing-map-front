c76e13477cd1344ca8caec49f3add0978332fb39
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-06-11 08:44:16 +0900

    Update nowLocation

--- www/js/map.js
+++ www/js/map.js
@@ -2439,45 +2439,6 @@ for (let i = 0; i < locations.length; i++) {
 
 let mapInfo;
 
-
-// 現在地取得処理
-function getPosition() {
-    // 現在地を取得
-    navigator.geolocation.getCurrentPosition(
-        // 取得成功した場合
-        function (position) {
-            // alert("緯度:" + position.coords.latitude + ",経度" + position.coords.longitude);
-        },
-        // 取得失敗した場合
-        function (error) {
-            switch (error.code) {
-                case 1: //PERMISSION_DENIED
-                    // alert("位置情報の利用が許可されていません");
-                    break;
-                case 2: //POSITION_UNAVAILABLE
-                    // alert("現在位置が取得できませんでした");
-                    break;
-                case 3: //TIMEOUT
-                    // alert("タイムアウトになりました");
-                    break;
-                default:
-                    // alert("その他のエラー(エラーコード:" + error.code + ")");
-                    break;
-            }
-        }
-    );
-}
-
-// Geolocation APIに対応している
-if (navigator.geolocation) {
-    getPosition();
-    // alert("この端末では位置情報が取得できます");
-    // Geolocation APIに対応していない
-} else {
-    // alert("この端末では位置情報が取得できません");
-}
-
-
 function initMap() {
     // 地図の作成
     mapInfo = new google.maps.LatLng({
@@ -2538,6 +2499,46 @@ function initMap() {
     });
 }
 
+// 現在地取得処理
+function getPosition() {
+    // 現在地を取得
+    navigator.geolocation.getCurrentPosition(
+        // 取得成功した場合
+        function (position) {
+            // alert("緯度:" + position.coords.latitude + ",経度" + position.coords.longitude);
+            let nowLat = position.coords.latitude;
+            let nowLng = position.coords.longitude;
+            map.panTo(new google.maps.LatLng(nowLat, nowLng));
+        },
+        // 取得失敗した場合
+        function (error) {
+            switch (error.code) {
+                case 1: //PERMISSION_DENIED
+                    // alert("位置情報の利用が許可されていません");
+                    break;
+                case 2: //POSITION_UNAVAILABLE
+                    // alert("現在位置が取得できませんでした");
+                    break;
+                case 3: //TIMEOUT
+                    // alert("タイムアウトになりました");
+                    break;
+                default:
+                    // alert("その他のエラー(エラーコード:" + error.code + ")");
+                    break;
+            }
+        }
+    );
+}
+
+// Geolocation APIに対応している
+if (navigator.geolocation) {
+    getPosition();
+    // alert("この端末では位置情報が取得できます");
+    // Geolocation APIに対応していない
+} else {
+    // alert("この端末では位置情報が取得できません");
+}
+
 
 let showLoadingDialog = function () {
     let loadingDialog = document.getElementById('loading-dialog');
@@ -2587,3 +2588,8 @@ get(REQUEST_URL)
         //エラー処理を記述する
         console.error(error);
     })
+
+
+const getNowLocation = function(){
+    getPosition();
+}
