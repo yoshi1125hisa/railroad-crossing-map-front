abc4e161e28d6674906128ad2de5a44b306c5b75
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-16 23:16:33 +0900

    update: commitizenの導入

---
+++ .cz-config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  types: [
+    { value: 'update', name: '新しい機能' },
+    { value: 'hotfix', name: 'バグフィックス' },
+    { value: 'fix', name: '改善' },
+    { value: 'other', name: 'その他' }
+  ]
+};
